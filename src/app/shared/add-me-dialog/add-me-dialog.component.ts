2ad60961c5b059fa185f598b17e17a1c65d5f3eb
MikhailZharkov <zharkov@abcreg.ru>
2018-09-10 00:25:27 +0300

    help

--- src/app/shared/add-me-dialog/add-me-dialog.component.ts
+++ src/app/shared/add-me-dialog/add-me-dialog.component.ts
@@ -9,15 +9,15 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 export class AddMeDialogComponent implements OnInit {
 
   DECK_CLASSES = [
-    { id: 'Druid', title: 'Друид', select: false },
-    { id: 'Mage', title: 'Маг', select: false },
-    { id: 'Paladin', title: 'Паладин', select: false },
-    { id: 'Priest', title: 'Жрец', select: false },
-    { id: 'Rogue', title: 'Разбойник', select: false },
-    { id: 'Shaman', title: 'Шаман', select: false },
-    { id: 'Warlock', title: 'Чернокнижник', select: false },
-    { id: 'Warrior', title: 'Воин', select: false },
-    { id: 'Hunter', title: 'Охотник', select: false },
+    { id: 'Druid', title: 'Друид', select: false, extra: false },
+    { id: 'Mage', title: 'Маг', select: false, extra: false },
+    { id: 'Paladin', title: 'Паладин', select: false, extra: false },
+    { id: 'Priest', title: 'Жрец', select: false, extra: false },
+    { id: 'Rogue', title: 'Разбойник', select: false, extra: false },
+    { id: 'Shaman', title: 'Шаман', select: false, extra: false },
+    { id: 'Warlock', title: 'Чернокнижник', select: false, extra: false },
+    { id: 'Warrior', title: 'Воин', select: false, extra: false },
+    { id: 'Hunter', title: 'Охотник', select: false, extra: false },
   ];
 
   constructor(
@@ -31,10 +31,26 @@ export class AddMeDialogComponent implements OnInit {
   get isCorrect() {
     return this.DECK_CLASSES.filter(c => c.select).length === this.data.DeckCount;
   }
+  get selectedCount() {
+    return this.DECK_CLASSES.filter(c => c.select).length;
+  }
+  
 
   closeDialog() {
     if (this.isCorrect) {
-      this.dialogRef.close(this.DECK_CLASSES.filter(c => c.select).map(c => c.id));
+      this.dialogRef.close(this.DECK_CLASSES.filter(c => c.select).sort((a, b) => a.extra ? 1 : 0).map(c => c.id));
+    }
+  }
+
+  selectDeck(deck) {
+    if (deck.select) {
+      deck.extra = false;
+    }
+
+    deck.select = !deck.select;
+
+    if (this.DECK_CLASSES.filter(c => c.select).length === this.data.DeckCount && this.data.DeckCount === 4) {
+      deck.extra = true;
     }
   }
 
